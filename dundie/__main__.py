73f8ece4025a700f76faa410fceec7000f1e5da2
Tiago Yamaguti <108343765+YamagutiSenior@users.noreply.github.com>
2024-12-23 21:00:27 +0000

    commit >> 0003

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,3 +1,28 @@
+import os
+import argparse
+
+
 def main():
-    print("Hello, Running dundie...")
-    print(f"Current directory: {os.getcwd()}")
+    parser = argparse.ArgumentParser(
+    description='Dunder Mifflin Reward Point System',
+    epilog="Enjoy and earn rewards for your hard work!",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="Subcommand to run",
+        choices=("load", "show", "send"),
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="Path to the reward file",
+    )
+    args = parser.parse_args()
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
+    #print("Hello, Running dundie...")
+    #print(f"Current directory: {os.getcwd()}")
